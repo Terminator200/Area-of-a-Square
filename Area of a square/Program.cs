fe29cbe02ed0e264636d4f1352bbbd1f3fee150c
михаил галев <mgalev257@gmail.com>
2018-06-12 21:44:15 +0300

    Importing Project

---
+++ Area of a square/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_of_a_square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new string('*', n));
+        }
+    }
+}
