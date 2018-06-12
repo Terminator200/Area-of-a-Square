a9f228c68de39cbf718aa17f092095456ad80b61
михаил галев <mgalev257@gmail.com>
2018-06-12 22:04:37 +0300

    commit all

--- Area of a square/Program.cs
+++ Area of a square/Program.cs
@@ -13,6 +13,11 @@ namespace Area_of_a_square
             int n = int.Parse(Console.ReadLine());
 
             Console.WriteLine(new string('*', n));
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.WriteLine('*' + new string(' ', n - 2) + '*');
+            }
+            Console.WriteLine(new string('*', n));
         }
     }
 }
